022b661265c0ba8b6ddd95cafe78c8df3b192327
Michelle A. <loyalslytherindeatheater@gmail.com>
2025-11-10 20:01:58 +0100

    Rolemodel  erstellt

--- backend/models/role.model.js
+++ backend/models/role.model.js
@@ -2,3 +2,23 @@
 Definition der Rollen (Superadmin, Admin, Player, Guest).
 */
 
+import mongoose from "mongoose";
+
+const roleSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+      enum: ["superadmin", "admin", "player", "guest"]
+    },
+    description: { type: String },
+    permissions: {
+      type: [String],
+      default: []
+    }
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Role", roleSchema);
